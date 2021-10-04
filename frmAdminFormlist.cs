aaf5368fe7974d37ad2b24531168edd1244d1151
Kevin Montgomery <kevin.j.montgomery@census.gov>
2021-10-04 07:40:48 -0400

    CR 8523 - New screen to view Formlist table

---
+++ frmAdminFormlist.cs
@@ -0,0 +1,133 @@
+﻿/**********************************************************************************
+Econ App Name:      CPRS
+Project Name:       CPRS Interactive Screens System
+Program Name:       frmAdminFormlist.cs	    	
+Programmer:         Christine Zhang
+Creation Date:      09/28/2021
+Inputs:             None                                  
+Parameters:         None                 
+Outputs:            None
+Description:	    This program displays the Form list table 
+Detailed Design:    Detailed Design for form list
+Other:	            Called from: Parent screen menu Admin -> Formlist
+ 
+Revision:	
+***********************************************************************************
+ Modified Date :  
+ Modified By   :  
+ Keyword       :  
+ Change Request:  
+ Description   :  
+***********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using CprsBLL;
+using CprsDAL;
+using System.Drawing.Printing;
+using System.IO;
+using System.Collections;
+
+
+namespace Cprs
+{
+    public partial class frmAdminFormlist : frmCprsParent
+    {
+        public frmAdminFormlist()
+        {
+            InitializeComponent();
+        }
+
+        private FormListData dataObject;
+
+        private void frmAdminFormlist_Load(object sender, EventArgs e)
+        {
+            GeneralDataFuctions.AddCpraccessData("ADMINISTRATIVE", "ENTER");
+            GeneralDataFuctions.UpdateCurrentUsersData("ADMINISTRATIVE");
+
+            dataObject = new FormListData();
+            GetData();
+
+            //set up buttons
+
+            btnAdd.Enabled = false;
+            btnDelete.Enabled = false;
+
+            //Only HQ manager and programmer can do add and delete 
+            if (UserInfo.GroupCode == EnumGroups.HQManager || UserInfo.GroupCode == EnumGroups.Programmer || UserInfo.GroupCode == EnumGroups.HQAnalyst || UserInfo.GroupCode== EnumGroups.HQSupport)
+            {
+                btnAdd.Enabled = true;
+                if (dgData.Rows.Count > 0)
+                    btnDelete.Enabled = true;
+            }
+            
+        }
+
+        private void GetData()
+        {
+            DataTable dt = dataObject.GetFormlist();
+            dgData.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.EnableResizing;
+            dgData.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgData.DataSource = null;
+            dgData.DataSource = dt;
+            dgData.RowHeadersVisible = false;
+
+            dgData.Columns[0].HeaderText = "RESPID";
+            dgData.Columns[0].Width = 150;
+            dgData.Columns[1].HeaderText = "RESPONDENT ORGANIZATION";
+            dgData.Columns[1].Width = 400;
+            dgData.Columns[2].HeaderText = "USER";
+            dgData.Columns[2].Width = 100;
+            dgData.Columns[3].HeaderText = "DATE/TIME";
+            dgData.Columns[3].Width = 150;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmRespidPopup popup = new frmRespidPopup(false, false, true);
+            popup.StartPosition = FormStartPosition.CenterParent;
+            popup.ShowDialog();  //show child
+
+            //if the popup was cancelled, set status to old value
+            if (popup.DialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                string respid = popup.NewRespid;
+                if (!dataObject.CheckExistinFormlist(respid))
+                {
+                    //Add to database
+                    dataObject.AddFormlist(respid);
+                    GetData();
+                    if (dgData.Rows.Count > 0 && !btnDelete.Enabled )
+                        btnDelete.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("The respid entered already exists in the form list table.");
+                }
+
+            }
+            popup.Dispose();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+           string respid = dgData.CurrentRow.Cells[0].Value.ToString();  
+           DialogResult dr = MessageBox.Show("Are you sure you want to delete Respid " + respid + " ?",
+                      "Question", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                //delete from database 
+                dataObject.DeleteFormlist(respid);
+                GetData();
+
+                if (dgData.Rows.Count == 0)
+                    btnDelete.Enabled = false;
+            }
+        }
+    }
+}
---
+++ frmAdminFormlist.designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Cprs
+{
+    partial class frmAdminFormlist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgData = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnDelete.Location = new System.Drawing.Point(634, 794);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(133, 23);
+            this.btnDelete.TabIndex = 46;
+            this.btnDelete.TabStop = false;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnAdd.Location = new System.Drawing.Point(429, 794);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(140, 23);
+            this.btnAdd.TabIndex = 45;
+            this.btnAdd.TabStop = false;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dgData
+            // 
+            this.dgData.AllowUserToAddRows = false;
+            this.dgData.AllowUserToDeleteRows = false;
+            this.dgData.AllowUserToResizeColumns = false;
+            this.dgData.AllowUserToResizeRows = false;
+            this.dgData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgData.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgData.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgData.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgData.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgData.Location = new System.Drawing.Point(165, 111);
+            this.dgData.MultiSelect = false;
+            this.dgData.Name = "dgData";
+            this.dgData.ReadOnly = true;
+            this.dgData.RowHeadersVisible = false;
+            this.dgData.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.dgData.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgData.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.dgData.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            this.dgData.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dgData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgData.Size = new System.Drawing.Size(859, 564);
+            this.dgData.TabIndex = 44;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label2.Location = new System.Drawing.Point(526, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 25);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "FORM LIST";
+            // 
+            // frmAdminFormlist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 861);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgData);
+            this.Controls.Add(this.label2);
+            this.Name = "frmAdminFormlist";
+            this.Load += new System.EventHandler(this.frmAdminFormlist_Load);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.dgData, 0);
+            this.Controls.SetChildIndex(this.btnAdd, 0);
+            this.Controls.SetChildIndex(this.btnDelete, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgData;
+        private System.Windows.Forms.Label label2;
+    }
+}
