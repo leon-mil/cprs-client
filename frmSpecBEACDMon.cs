7b83410ac92b68d1a428f1560546e9d926e4ad77
Kevin Montgomery <kevin.j.montgomery@census.gov>
2023-03-02 11:22:46 -0500

    CR 885 - Update Excel

--- frmSpecBEACDMon.cs
+++ frmSpecBEACDMon.cs
@@ -13,11 +13,11 @@ Detailed Design : Detailed Design for special bea Monthly
 Other           :	            
 Revision History:	
 **************************************************************************************************
-Modified Date   :  
-Modified By     :  
-Keyword         :  
-Change Request  :  
-Description     :  
+ Modified Date : 2/21/2023
+ Modified By   : Christine Zhang
+ Keyword       : 
+ Change Request: CR885
+ Description   : update excel file name from .xls to .xlsx
 **************************************************************************************************/
 using System;
 using System.Collections.Generic;
@@ -204,9 +204,9 @@ namespace Cprs
         {
             // Displays a SaveFileDialog save file
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.Filter = "Excel file|*.xls";
+            saveFileDialog1.Filter = "Excel file|*.xlsx";
             saveFileDialog1.Title = "Save an File";
-            saveFileDialog1.FileName = "FedBEAMon.xls";
+            saveFileDialog1.FileName = "FedBEAMon.xlsx";
 
             var result = saveFileDialog1.ShowDialog();
 
@@ -219,7 +219,7 @@ namespace Cprs
             //delete exist file
             FileInfo fileInfo = new FileInfo(saveFilename);
             string dir = MapNetwork.Pathing.GetUNCPath(fileInfo.DirectoryName);
-            //if (GeneralFunctions.IsFileinUse(dir + "\\FedBEAAnn.xls"))
+            //if (GeneralFunctions.IsFileinUse(dir + "\\FedBEAAnn.xlsx"))
             //{
             //    MessageBox.Show(saveFilename + " is in use. Please close it.");
             //    return;
@@ -333,7 +333,7 @@ namespace Cprs
         {
             FileInfo fileInfo = new FileInfo(saveFilename);
             string dir = MapNetwork.Pathing.GetUNCPath(fileInfo.DirectoryName);
-            string sfilename = dir + "\\FedBEAMon.xls";
+            string sfilename = dir + "\\FedBEAMon.xlsx";
 
             /*start a new thread */
             Thread t = new Thread(new ThreadStart(Splashstart));
@@ -348,7 +348,7 @@ namespace Cprs
             
             // Save file & Quit application
             xlApp.DisplayAlerts = false; //Supress overwrite request
-            xlWorkBook.SaveAs(sfilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.SaveAs(sfilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
             xlWorkBook.Close(true, misValue, misValue);
             xlApp.Quit();
 
